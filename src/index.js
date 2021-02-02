f9984b01b81c9ad067c90440f10d78bd6ffb5073
2lembre <2lembre@gmail.com>
2021-02-02 14:41:59 -0800

    addition basic files with photo anubis in js with css basic

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './styles.css';
+import Icon from './assets/anubis.png';
+
+function article() {
+  const article = document.createElement('article');
+  const title = document.createElement('h1');
+  const imageAnubis = new Image();
+  const name = 'Anúbis';
+
+  article.classList.add('anubis');
+  title.innerHTML = name;
+  imageAnubis.alt = `${name} photo`;
+  imageAnubis.src = Icon;
+  imageAnubis.classList.add('anubis-photo');
+
+  article.appendChild(title);
+  article.appendChild(imageAnubis);
+
+  return article;
+}
+
+document.body.prepend(article());
