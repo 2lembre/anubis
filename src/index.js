00030ac76a6f617dedb5a5048ba45fb66603da7e
2lembre <2lembre@gmail.com>
2021-02-03 04:20:19 -0800

    effect hover 3D with javascript and CSS

--- src/index.js
+++ src/index.js
@@ -20,3 +20,22 @@ function articleComponent() {
 }
 
 document.body.prepend(articleComponent());
+
+let anubis = document.querySelector('.anubis');
+let screenWidth = window.screen.width / 2;
+let screenHeight = window.screen.height / 2;
+let otherProperty = 'perspective(1000px)';
+
+document.addEventListener('mousemove', function (e) {
+  let centroX = e.clientX - screenWidth;
+  let centroY = screenHeight - e.clientY;
+  let degX = centroX * 0.04;
+  let degY = centroY * 0.08;
+
+  anubis.style.transform = otherProperty + 'rotateY('+ degX +'deg)  rotateX('+ degY +'deg)';
+});
+
+// O seu coração contra peso nessa "pena da verdade"
+// Está cheio de maldade ou será que é de bondade?
+// 2Lembre a sinceridade trabalhe sempre com a verdade.
+// 03/02/20/21
