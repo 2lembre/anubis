9d9b5e7d9b6a0ded13a1b1c5249086930e60a956
2lembre <2lembre@gmail.com>
2021-02-02 17:40:50 -0800

    adjustment meta tags and favicons

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,25 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const nameApp = 'Anúbis'
+
+let htmlConfig = {
+  hash: true,
+  title: nameApp,
+  favicon: './src/favicon.ico',
+  template: './src/index.html',
+  filename: 'index.html',
+  meta: {
+    'author': '2lembre',
+    'viewport': 'width=device-width, initial-scale=1.0',
+    'application-name': nameApp,
+    'msapplication-config': process.env.NODE_ENV === 'production' ? '/anubis/browserconfig.xml' : '/browserconfig.xml',
+    'msapplication-TileColor': '#000000',
+    'theme-color': '#ffffff'
+  }
+}
 
 module.exports = {
   entry: './src/index.js',
@@ -14,14 +33,23 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: './src/index.html',
-      filename: 'index.html'
+    new HtmlWebpackPlugin(process.env.NODE_ENV === 'production' ? Object.assign(htmlConfig, { pathAnubis: '/anubis' }) : htmlConfig),
+    new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/manifest.json",
+          to: "manifest.json",
+          transform(content) { return process.env.NODE_ENV === 'production' ? content : content.toString().replace(/anubis\//gi, '') },
+        },
+        {
+          from: "src/browserconfig.xml",
+          to: "browserconfig.xml",
+          transform(content) { return process.env.NODE_ENV === 'production' ? content : content.toString().replace(/anubis\//gi, '') },
+        },
+        { from: "src/assets", to: "assets" },
+      ],
     }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css'
-    })
   ],
   module: {
     rules: [
